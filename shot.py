671128cfe9fbd8d43179884371b486787298076e
Yohey Funada <dev@fulufulu.com>
2018-12-27 21:36:12 +0900

    shot

---
+++ shot.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+import picamera
+
+with picamera.PiCamera() as camera:
+ camera.resolution = (1024, 768)
+ camera.start_preview()
+ # Camera warm-up time
+ time.sleep(2)
+ camera.capture('my_picture.jpg')
