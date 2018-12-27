a9ad85ce37afb8b39e782803f1d1b183c1a0419b
Yohey Funada <dev@fulufulu.com>
2018-12-27 22:02:22 +0900

    カメラ設定

--- shot.py
+++ shot.py
@@ -6,6 +6,22 @@ import picamera
 from datetime import datetime
 
 with picamera.PiCamera() as camera:
+ camera.sharpness = 0
+ camera.contrast = 0
+ camera.brightness = 50
+ camera.saturation = 0
+ camera.ISO = 0
+ camera.video_stabilization = False
+ camera.exposure_compensation = 0
+ camera.exposure_mode = 'auto'
+ camera.meter_mode = 'average'
+ camera.awb_mode = 'auto'
+ camera.image_effect = 'none'
+ camera.color_effects = None
+ camera.rotation = 0
+ camera.hflip = False
+ camera.vflip = False
+ camera.crop = (0.0, 0.0, 1.0, 1.0)
  camera.resolution = (1024, 768)
  camera.start_preview()
  # Camera warm-up time
